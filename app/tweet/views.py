af0a2c220302fc22319f714a32db906224523d2f
ValentinaLV <tina.lysenok@gmail.com>
2020-06-02 19:17:26 +0300

    added tweets view

--- app/tweet/views.py
+++ app/tweet/views.py
@@ -5,6 +5,7 @@ from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 from app.user.models import User
+from app.user.models import followers
 from . import tweet_page
 from .forms import TweetForm
 from .models import Tweet
@@ -29,6 +30,15 @@ def time_since(delta):
         return 'Just now'
 
 
+@tweet_page.route('/')
+def tweets():
+    tweets_all = Tweet.query.all()
+    current_time = datetime.now()
+    return render_template('tweet/tweets.html',
+                           tweets=tweets_all,
+                           current_time=current_time)
+
+
 @tweet_page.route('/timeline', defaults={'username': None})
 @tweet_page.route('/timeline/<username>')
 @login_required
@@ -39,11 +49,14 @@ def timeline(username):
         user = User.query.filter_by(username=username).first()
         if not user:
             abort(404)
+        tweets = Tweet.query.filter_by(user=user) \
+            .order_by(Tweet.created.desc()).all()
     else:
         user = current_user
+        tweets = Tweet.query.join(followers, (followers.c.followee_id == Tweet.user_id)) \
+            .filter(followers.c.follower_id == current_user.id) \
+            .order_by(Tweet.created.desc()).all()
 
-    tweets = Tweet.query.filter_by(user=user) \
-        .order_by(Tweet.created.desc()).all()
     current_time = datetime.now()
     return render_template('tweet/timeline.html',
                            form=form,
